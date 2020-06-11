1bd486ca91d3af146b2d3803679b4f22c2dd32ed
goodluck3586 <52552928+goodluck3586@users.noreply.github.com>
2020-06-12 08:42:25 +0900

    GG

---
+++ Roll a Ball Tutorial/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 10.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");  // 키보드 왼쪽/오른쪽 화살표 입력값
+        float v = Input.GetAxis("Vertical");    // 키보드 위/아래
+        //print($"h = {h}, v = {v}");
+
+        Vector3 movement = new Vector3(h, 0f, v);
+
+        rb.AddForce(movement * speed);
+    }
+}
